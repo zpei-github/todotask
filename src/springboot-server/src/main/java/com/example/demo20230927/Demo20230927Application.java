d79f69050df445da9ebadd1cc43b91c2bd80c510
zpyyds666666 <zpei123@outlook.com>
2024-04-26 20:11:36 +0800

    第三次

--- src/springboot-server/src/main/java/com/example/demo20230927/Demo20230927Application.java
+++ src/springboot-server/src/main/java/com/example/demo20230927/Demo20230927Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo20230927Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Demo20230927Application.class, args);
 	}
-
 }
